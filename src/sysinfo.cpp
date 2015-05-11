ce32a875d7bff1a896ddb3855c96f948bb49d914
Leszek Lesner <leszek.lsner@web.de>
2015-05-11 10:03:10 +0200

    Use os-release for grabbing os infos instead of lsb-release

--- src/sysinfo.cpp
+++ src/sysinfo.cpp
@@ -751,7 +751,7 @@ void kio_sysinfoProtocol::osInfo()
 #elif defined(WITH_DEBIAN)
     m_info[ OS_SYSTEM ] = readFromFile( "/etc/debian_version" );
 #elif defined(WITH_UBUNTU)
-    m_info[ OS_SYSTEM ] = readFromFile ( "/etc/lsb-release", "DISTRIB_ID", "=" ) + " " + readFromFile ( "/etc/lsb-release", "DISTRIB_RELEASE", "=" );
+    m_info[ OS_SYSTEM ] = readFromFile ( "/etc/os-release", "NAME", "=" ) + " " + readFromFile ( "/etc/os-release", "VERSION", "=" );
 #else
     m_info[ OS_SYSTEM ] = i18nc( "Unknown operating system version", "Unknown" );
 #endif
